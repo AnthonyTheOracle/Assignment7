d9ba8ae65ddf2f40aa692795c8b78c866594c34f
Anthony McCofie <AnthonyTheOracle@outlook.com>
2022-02-27 20:58:52 -0500

    updated @ 8:58

---
+++ Assignment7.R
@@ -0,0 +1,16 @@
+
+#data.gov API was used to download current smoking among adults in 2015 (national)
+dataNew <- data.frame(read.csv("C:\\USF\\SPRING2022\\R-PROGRAMMING\\Assignment7\\tobaccoUsage.csv"))
+
+data <- head(dataNew$YEAR, dataNew$LocationDesc, dataNew$Data_Value, dataNew$Age, dataNew$Gender)
+
+data
+
+
+setClass("employee", slots=list(name="character", age="numeric", salary="numeric"))
+
+emp <- new("employee",name="Anthony", age=42, salary=500)
+
+isS4(emp)
+
+!isS4(emp)
